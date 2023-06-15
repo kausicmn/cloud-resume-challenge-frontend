8cb21b10d6ce556a24a480c1d954da7e0f6991d6
kausicmn <92553246+kausicmn@users.noreply.github.com>
2023-06-15 14:25:40 -0700

    Added backend s3 configuration

--- provider.tf
+++ provider.tf
@@ -5,11 +5,11 @@ terraform {
         version= "5.2.0"
     }
   }
-  #   backend "s3" {
-  #   bucket = "tf-state-frontend-kausic"
-  #   key    = "terraform-state"
-  #   region = "us-east-1"
-  # }
+    backend "s3" {
+    bucket = "tf-state-frontend-kausic"
+    key    = "terraform-state"
+    region = "us-east-1"
+  }
 }
 provider "aws" {
     region = "us-east-1"
